75b3ca4bf71745865ad4d5f66e64c788732b4aea
ws <56677272+2276089666@users.noreply.github.com>
2021-04-15 22:42:03 +0800

    暴力递归求解贪心问题

--- algorithms/greedy/LexicalOrderSmallest.java
+++ algorithms/greedy/LexicalOrderSmallest.java
@@ -1,7 +1,9 @@
 package greedy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 
 /**
  * @Author ws
@@ -36,9 +38,44 @@ public class LexicalOrderSmallest {
         }
     }
 
+    // 暴力递归，把所有的情况列出来
+    public static String lowestString1(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        ArrayList<String> all = new ArrayList<>();
+        HashSet<Integer> use = new HashSet<>();
+        process(strs, use, "", all);
+        String lowest = all.get(0);
+        for (int i = 1; i < all.size(); i++) {
+            if (all.get(i).compareTo(lowest) < 0) {
+                lowest = all.get(i);
+            }
+        }
+        return lowest;
+    }
+
+    public static void process(String[] strs, HashSet<Integer> use, String path, ArrayList<String> all) {
+        if (use.size() == strs.length) {
+            all.add(path);
+        } else {
+            for (int i = 0; i < strs.length; i++) {
+                if (!use.contains(i)) {
+                    use.add(i);
+                    process(strs, use, path + strs[i], all);
+                    use.remove(i);
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         String[] strs =new String[]{"ac","b","ba"};
         String string = lowestString(strs);
         System.out.println(string);
+
+        String s = lowestString1(strs);
+        System.out.println(s);
     }
 }
