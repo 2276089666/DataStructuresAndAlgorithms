95238b838a558e33d3bde764e2ab18e7283cd950
ws <56677272+2276089666@users.noreply.github.com>
2021-04-22 20:36:11 +0800

    多样本位置全对应的尝试模型

---
+++ algorithms/violentRecursion/multipleSampleLocations/CommonSubsequenceOfTwoStrings.java
@@ -0,0 +1,57 @@
+package violentRecursion.multipleSampleLocations;
+
+
+/**
+ * @Author ws
+ * @Date 2021/4/22 14:04
+ * @Version 1.0
+ */
+public class CommonSubsequenceOfTwoStrings {
+    /**
+     * 多样本位置全对应的尝试模型:
+     * 两个字符串的最长公共子序列问题
+     */
+    public static int getCommonSubsequenceOfTwoStrings(String a,String b){
+        if (a==null||b==null||"".equals(a)||"".equals(b)){
+            return 0;
+        }
+        char[] array1 = a.toCharArray();
+        char[] array2 = b.toCharArray();
+        return lcse(array1,array2);
+    }
+
+    // 动态规划
+    // 利用二维数组行代表str1的index,列代表str2的index,如果str1的3对于str2的2相等，我们的dp[2][1]=1
+    public static int lcse(char[] str1, char[] str2) {
+        int[][] dp = new int[str1.length][str2.length];
+        // baseCase
+        dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+        // 相等的部分不以str1结尾
+        for (int i = 1; i < str1.length; i++) {
+            // 串的前面部分某个字符已经相等，后面的这行或这列必定为1
+            dp[i][0] = Math.max(dp[i - 1][0], str1[i] == str2[0] ? 1 : 0);
+        }
+        // 相等的部分不以str2结尾
+        for (int j = 1; j < str2.length; j++) {
+            dp[0][j] = Math.max(dp[0][j - 1], str1[0] == str2[j] ? 1 : 0);
+        }
+        for (int i = 1; i < str1.length; i++) {
+            for (int j = 1; j < str2.length; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                // 相等的部分不仅以str1结尾并且也以str2结尾
+                if (str1[i] == str2[j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[str1.length - 1][str2.length - 1];
+    }
+
+
+    public static void main(String[] args) {
+        String a="1890238kklasd";
+        String b="asdpoqp897231";
+        int commonSubsequenceOfTwoStrings = getCommonSubsequenceOfTwoStrings(a, b);
+        System.out.println(commonSubsequenceOfTwoStrings);
+    }
+}
