bbd9719a22f9c7960330f970082057a1ef548e24
ws <56677272+2276089666@users.noreply.github.com>
2021-05-18 21:45:54 +0800

    刷题开始,day01

---
+++ exam/interview/Exam_01.java
@@ -0,0 +1,67 @@
+package interview;
+
+/**
+ * @Author ws
+ * @Date 2021/5/18 18:46
+ * @Version 1.0
+ */
+public class Exam_01 {
+    /**
+     * 给定一个有序数组arr，从左到右依次表示X轴上从左往右点的位置
+     * 给定一个正整数K，返回如果有一根长度为K的绳子，最多能盖住几个点
+     * 绳子的边缘点碰到X轴上的点，也算盖住
+     */
+
+    // 暴力从下标0一个个向右尝试  O(N^2)
+    public static int getMax(int[] arr,int k){
+        if (arr==null||arr.length==0||k<=0){
+            return 0;
+        }
+        int result=0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp=0;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]-arr[i]>k){
+                    break;
+                }
+                temp++;
+            }
+            result=Math.max(result,temp+1);
+        }
+        return result;
+    }
+
+
+    // 看到有单调性,立马使用窗口   O(N)
+    public static int getMax2(int[] arr,int k){
+        if (arr==null||arr.length==0||k<=0){
+            return 0;
+        }
+        int result=0;
+        int L=0;
+        int R=0;
+        int distance=0;
+        while (R<arr.length){
+            if (distance<=k){
+                R++;
+                if (R==arr.length){
+                    result=Math.max(result,R-L);
+                    break;
+                }
+                distance=arr[R]-arr[L];
+            }else  {
+                result=Math.max(result,R-L);
+                distance=arr[R]-arr[++L];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-4, -1, 2, 3, 6, 7, 10,11,12};
+        int max = getMax(arr, 6);
+        System.out.println(max);
+        int max2 = getMax2(arr, 6);
+        System.out.println(max2);
+    }
+}
---
+++ exam/interview/Exam_02.java
@@ -0,0 +1,88 @@
+package interview;
+
+import java.util.Stack;
+
+/**
+ * @Author ws
+ * @Date 2021/5/18 20:08
+ * @Version 1.0
+ */
+public class Exam_02 {
+    /**
+     * 括号有效配对是指：
+     * 1）任何一个左括号都能找到和其正确配对的右括号
+     * 2）任何一个右括号都能找到和其正确配对的左括号
+     * 有效的：    (())  ()()   (()())  等
+     * 无效的：     (()   )(     等
+     * 问题一：怎么判断一个括号字符串有效？
+     * 问题二：如果一个括号字符串无效，返回至少填几个字符能让其整体有效
+     */
+
+    // 问题一   使用栈去实现
+    public static boolean getInEffect(String str){
+        if (str==null||"".equals(str)){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]=='('){
+                stack.push(array[i]);
+            }
+            if (array[i]==')'){
+                if (!stack.isEmpty()){
+                    stack.pop();
+                }else {
+                    return false;
+                }
+
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    // 问题一  优化使用一个count变量
+    public static boolean getInEffect2(String str){
+        if (str==null||"".equals(str)){
+            return false;
+        }
+        int count=0;
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+           count+=array[i]=='('?1:-1;
+           if (count<0){
+               return false;
+           }
+        }
+        return count==0;
+    }
+
+    // 问题二 还是用两个变量
+    public static int getNum(String str){
+        if (str==null||"".equals(str)){
+            return -1;
+        }
+        int count=0;
+        int need=0;
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            count+=array[i]=='('?1:-1;
+            if (count<0){
+                need++;
+                count=0;
+            }
+        }
+        return count+need;
+    }
+
+    public static void main(String[] args) {
+        String str="(())()((()))";
+        boolean inEffect = getInEffect(str);
+        System.out.println(inEffect);
+        boolean inEffect2 = getInEffect2(str);
+        System.out.println(inEffect2);
+        String str2="))((()())";
+        int num = getNum(str2);
+        System.out.println(num);
+    }
+}
