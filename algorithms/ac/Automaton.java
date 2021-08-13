848254063feb5fd91e417250c6623f8cef26521e
ws <56677272+2276089666@users.noreply.github.com>
2021-08-13 10:36:53 +0800

    AC自动机

---
+++ algorithms/ac/Automaton.java
@@ -0,0 +1,48 @@
+package ac;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author ws
+ * @Date 2021/8/13 10:35
+ */
+public class Automaton {
+    /**
+     * leetcode 第8题AC自动机
+     */
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    // 状态表（重中之重）
+    private Map<String, String[]> table = new HashMap<String, String[]>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});
+    }};
+
+    public void get(char c) {
+        // 从上一个状态根据扫描到的字符更新下一个状态
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
+        }
+    }
+
+    private int get_col(char c) {
+        if (c == ' ') {
+            return 0;
+        }
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        if (Character.isDigit(c)) {
+            return 2;
+        }
+        return 3;
+    }
+}
